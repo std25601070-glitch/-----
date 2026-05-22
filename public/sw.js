a003564bd7efa9ee8c952a0a8f0cd295e11f103d
std25601070-glitch <std25601070@uomus.edu.iq>
2026-05-22 15:11:22 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -41,3 +41,29 @@ self.addEventListener('fetch', (event) => {
     })
   )
 })
+// الاستماع لحدث وصول الإشعار من السيرفر
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  
+  const options = {
+    body: data.body,
+    icon: '/icons/icon-192x192.png', // غيره لمسار لوجو مشروعك
+    badge: '/icons/badge.png',        // أيقونة صغيرة تظهر في شريط التنبيهات علوياً
+    vibrate: [200, 100, 200],          // نمط الاهتزاز في الهواتف
+    data: {
+      url: '/' // الصفحة التي سيفتحها المستخدم عند الضغط على الإشعار
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+// فتح الموقع عند الضغط على الإشعار
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
