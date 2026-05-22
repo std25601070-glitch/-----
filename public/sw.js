a9e5d3886016f14325b218dbb19df119e7d5eb93
std25601070-glitch <std25601070@uomus.edu.iq>
2026-05-22 13:37:39 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'nurai-v1'
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icon-light-32x32.png',
+  '/apple-icon.png',
+]
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+  self.skipWaiting()
+})
+
+// Activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response
+      return fetch(event.request).catch(() => {
+        return caches.match('/')
+      })
+    })
+  )
+})
